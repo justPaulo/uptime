55bc7e50d1168ba4f07f611cecfea56948a8c77b
justPaulo <pnascim@gmail.com>
2017-09-18 17:26:09 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms; // só para poder suportar a caixa de diálogo básica
+
+namespace uptime
+{
+    class Program
+    {
+        public static string ConvertSecondsToDate(string seconds)
+        {
+            TimeSpan t = TimeSpan.FromSeconds(Convert.ToDouble(seconds));
+
+             if (t.Days > 0)
+                return t.ToString(@"d\d\,\ hh\:mm\:ss");
+
+            return t.ToString(@"hh\:mm\:ss");
+        }
+
+        public static int getUpTime()
+        {
+            PerformanceCounter pc = new PerformanceCounter("System", "System Up Time");
+            pc.NextValue();
+            int uptime = (int)pc.NextValue();
+
+            return uptime;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Welcome to Uptime system utility - the missing NT command...";
+
+            if (args.Length > 0)
+            {
+                if (args[0] == "-g")
+                    { DialogResult res = MessageBox.Show(ConvertSecondsToDate(getUpTime().ToString()), "Uptime", MessageBoxButtons.OK, MessageBoxIcon.Information); }
+
+                if (args[0] == "-t")
+                    Console.WriteLine("Uptime: {0}", ConvertSecondsToDate(getUpTime().ToString()));
+            }
+            else
+                Console.WriteLine("\nUSAGE: uptime [-g] ^ [-t]");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("uptime")]
+[assembly: AssemblyDescription("An uptime simple utility")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("uptime")]
+[assembly: AssemblyCopyright("Copyright ©  2017 Paulo A. Nascimento")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f51aa12e-244a-405e-9054-d3371fa9821c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
